6f12a689c6a306213676cdcb74d24c75683da547
Paresh Dudhat <dudhatparesh@gmail.com>
2020-11-06 11:25:51 +0530

    json data fetch

--- app/src/main/java/com/example/apicalling/ui/main/MainActivity.kt
+++ app/src/main/java/com/example/apicalling/ui/main/MainActivity.kt
@@ -2,25 +2,57 @@ package com.example.apicalling.ui.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
 import com.example.apicalling.R
+import com.example.apicalling.data.PostModel
+import com.example.apicalling.network.RetrofitInterface
 import kotlinx.android.synthetic.main.activity_main.*
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
-class MainActivity : AppCompatActivity(), MainView {
-    lateinit var presenter: MainPresenter
+class MainActivity : AppCompatActivity() {
+    //lateinit var presenter: MainPresenter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        presenter = MainPresenter()
-        presenter.attachView(this)
-        presenter.getTodoDetail(1)
-        presenter.getbuttonText(2)
-    }
 
-    override fun displayTitle(title: String) {
-        tvTitle.text = title
-    }
 
-    override fun buttonText(btn: String) {
-        btv.text = btn
-    }
+        //in Retrofit.Builder we can call the function  of the Base_URL and the convertofactory that we want to define
+        // Base_URL define in the RetrofitInterface in which we can extract the data
+        var rf = Retrofit.Builder()
+            .baseUrl(RetrofitInterface.Base_URL)
+            .addConverterFactory(GsonConverterFactory.create()).build()
+
+
+        // creating API
+        //vairable API is initialize
+        var API : RetrofitInterface = rf.create(RetrofitInterface:: class.java)
+        // creating Call variable
+        var call = API.posts
+
+
+        //defining onResponse and
+        call?.enqueue(object:Callback<List<PostModel?>?>{
+            override fun onFailure(call: Call<List<PostModel?>?>, t: Throwable) {
+            }
+
+            override fun onResponse(
+                call: Call<List<PostModel?>?>,
+                response: Response<List<PostModel?>?>
+            ) {
+               var postlist : List<PostModel>? = response.body() as List<PostModel>
+                var post : Array<String?> = arrayOfNulls<String>(postlist!!.size)
+
+                for(i in postlist !!.indices)
+                    post[i] = postlist!![i]!!.title
+                var adapter = ArrayAdapter<String>(applicationContext,android.R.layout.simple_dropdown_item_1line, post)
+                listview.adapter = adapter
+
+            }
+
+        })
+}
 }
